2fd2fe491e7f4dba2bef7faa71cdbfcfddcfc397
Rishi Krishna <rishi1.krishna1@gmail.com>
2024-09-30 10:47:36 -0500

    Chapter 4.1: ownership.

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\projects\rust_practice\ownership\target\debug\deps\ownership.exe: src/main.rs
+
+C:\projects\rust_practice\ownership\target\debug\deps\ownership.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\projects\rust_practice\ownership\target\debug\ownership.exe: C:\projects\rust_practice\ownership\src\main.rs
