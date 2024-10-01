fcf86bcae7351e64d287ba4149de2fede1a07185
Rishi Krishna <rishi1.krishna1@gmail.com>
2024-10-01 12:03:48 -0500

    Chapter 4.3: slice.

---
+++ slice/target/debug/deps/slice.d
@@ -0,0 +1,5 @@
+C:\projects\rust_practice\slice\target\debug\deps\slice.exe: src/main.rs
+
+C:\projects\rust_practice\slice\target\debug\deps\slice.d: src/main.rs
+
+src/main.rs:
---
+++ slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+C:\projects\rust_practice\slice\target\debug\slice.exe: C:\projects\rust_practice\slice\src\main.rs
