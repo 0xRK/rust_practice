d793559ce48e0f481a2a6a13d223e074f39b8d65
Rishi Krishna <rishi1.krishna1@gmail.com>
2024-12-24 17:19:08 -0600

    Chapter 10.1: generic_data_types.

---
+++ generic_data_types/src/main.rs
@@ -0,0 +1,65 @@
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<T2, U2>(self, other: Point<T2, U2>) -> Point<T, U2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+
+    println!("The largest char is {result}");
+
+    let p = Point { x: 5.0, y: 10.0 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+
+    println!("p.x = {}", p.x());
+
+    println!("p.x = {}", p.distance_from_origin());
+
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
